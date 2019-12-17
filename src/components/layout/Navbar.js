6343f6ec8b9a36e0369807414d7abcf2bccd42de
Adakai <abovethenegativity23@gmail.com>
2019-12-17 08:30:38 -0700

    setup react and set up sass

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,9 @@
+import React from '../../../node_modules/react';
+// import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <div>hello</div>
+  );
+};
+export default Navbar;
