6b71a3830e0e7032959b71fcfd437c9933abc8a6
adakai <adakai@github.com>
2019-12-18 17:25:56 -0700

    got products displaying to page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,24 @@
 import React, { Fragment } from '../../../node_modules/react';
 import styles from './Navbar.module.scss';
+import logo from '../../logo.png'
 
 const Navbar = () => {
   return (
     <Fragment>
       <div id={styles.backgroundColor} className='navbar navbar-expand navbar-light'>
         <div className='container'>
-          <a className='navbar-brand' href='#'>
-            Swiftys
+          <a id={styles.bold} className='navbar-brand' href='/'>
+            <img id={styles.imgSize} src={logo} alt="Swift Commerce"/>
           </a>
           <div className='' id='navbarSupportedContent'>
             <ul className='navbar-nav ml-auto'>
               <li className='nav-item active'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   <i className="fas fa-home"></i>
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   <i className="fas fa-shopping-cart"></i>
                 </a>
               </li>
--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../../actions/getActions';
+import styles from './Products.module.scss';
 
 class Products extends Component {
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchProducts();
   }
 
   render() {
-    
-    const productItems = this.props.products.map(product => <div key={product.id}>{product.title}</div>);
+    const productItems = this.props.products.map(product => (
+      <Fragment key={product.id}>
+        <div id={styles.container} className='card' style={{ width: '18rem' }}>
+          <img id={styles.imgSize} src={product.img} className='card-img-top' alt='...' />
+          <div className='card-body d-flex flex-column justify-content-center align-items-center'>
+            <h5 className='card-title'>{product.title}</h5>
+            <p className='card-text'>{product.description}</p>
+            <h6>{product.price}</h6>
+            <button className='btn btn-primary'>Add to Cart</button>
+          </div>
+        </div>
+      </Fragment>
+    ));
+
     return (
-      <div>
-        <h1>products</h1>
+      <div className='container d-flex flex-wrap justify-content-center align-items-center'>
         {productItems}
       </div>
     );
