f0f215e25274e35c36e232079098e0e58270349f
Adakai <abovethenegativity23@gmail.com>
2020-01-06 10:09:25 -0700

    added unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -13,18 +13,8 @@ describe('App', () => {
       const div = document.createElement('div');
       ReactDOM.render(<App />, div);
     });
-    it('should have the `th` "Items"', () => {
-      expect(wrapper.contains(<th>Items</th>)).toBe(true);
-    });
-    it('should have a `button` element', () => {
-      expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(
-        true
-      );
-    });
-  });
-  describe('user interaction', () => {
-    it('should update the state property `item`', () => {
-      expect(wrapper.state().item).toEqual(item);
+    it('should have the a Navbar', () => {
+      expect(wrapper.contains(<Navbar />)).toBe(true);
     });
   });
 });
